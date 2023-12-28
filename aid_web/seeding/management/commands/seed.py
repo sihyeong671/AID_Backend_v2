6726e5d5100f429a3e9fd0d57ea2ec8c6d8afd7b
june222 <0119299@gmail.com>
2023-12-28 16:49:40 +0900

    feat: add superuser option
    - 새로 만든 userapp 폴더를 위해 import 변경
    
    - '--superuser' 라는 옵션을 만들어 is_admin을 True로 부여하고, 나머지는
      일반 동아리원으로 더미데이터 생성조건을 바꿈.
    
    - email, password, nickname은 코드 참조
    
    issue #8 #9

---
+++ aid_web/seeding/management/commands/seed.py
@@ -0,0 +1,61 @@
+from typing import Any
+
+from django.core.management.base import BaseCommand, CommandParser
+from django_seed import Seed
+from userapp.models import User
+
+
+# 예시: python manage.py seed_users --superuser=1 --user=10 --study=5
+class Command(BaseCommand):
+    help = "랜덤한 유저 데이터 생성을 이 커맨드를 활용해 할 수 있음"
+
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument("--superuser", default=0, type=int, help="몇 명의 슈퍼_유저 데이터를 만드나.")
+        parser.add_argument("--user", default=0, type=int, help="몇 명의 유저 데이터를 만드나.")
+        parser.add_argument("--study", default=0, type=int, help="몇 개의 스터디 데이터를 만드나.")
+
+    def handle(self, *args: Any, **options: Any) -> str | None:
+        user = options.get("user")
+        superuser = options.get("superuser")
+        # study = options.get("study")
+
+        # 한국 지역에 적용되는 값을 가져옴
+        # faker docs Link: https://faker.readthedocs.io/en/master/locales/ko_KR.html
+        seeder = Seed.seeder(locale="ko_KR")
+
+        # superuser 데이터를 만들 때는
+        seeder.add_entity(
+            User,
+            superuser,
+            {
+                "nickname": lambda x: "어드민",
+                "email": lambda x: "admin@cse.com",
+                "password": lambda x: "a123456789!",
+                "is_admin": lambda x: True,
+            },
+        )
+        # create super user
+
+        seeder.add_entity(
+            User,
+            user,
+            {
+                "nickname": lambda x: seeder.faker.name_female(),
+                "email": lambda x: seeder.faker.unique.email(),
+                "password": lambda x: seeder.faker.msisdn(),
+                "is_admin": lambda x: False,
+            },
+        )
+        # Django Seed 활용하여 FK 포함한 모델의 test data 생성하기
+        # Link: https://ninefloor-design.tistory.com/323
+        # seeder.add_entity(
+        #     Study,
+        #     study,
+        #     {
+        #         "study_name": lambda x: seeder.faker.bs(),
+        #         "study_description": lambda x: seeder.faker.catch_phrase(),
+        #         "study_link": lambda x: seeder.faker.url(),
+        #         "status": lambda x: randint(0, 2),
+        #     },
+        # )
+        seeder.execute()
