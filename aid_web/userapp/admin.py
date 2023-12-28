c0de26d2692a64b5bfd62280ce26c682b26d7f52
sihyeong671 <bshlab671@naver.com>
2023-12-28 13:23:30 +0900

    fix: docker file & admin file

--- aid_web/userapp/admin.py
+++ aid_web/userapp/admin.py
@@ -5,6 +5,6 @@ from . import models
 
 @admin.register(models.User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("email", "nickname", "created_at")
+    list_display = ("email", "nick_name", "created_at")
 
-    list_display_links = ("email", "nickname")
+    list_display_links = ("email", "nick_name")
