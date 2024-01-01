f1efb8bb9871ae9ce6e9100eb4d7af8b9b031088
june222 <0119299@gmail.com>
2024-01-02 01:42:37 +0900

    style: copy StudyModel from testapp to studyapp
    - testapp에서 작성했던 models.py를 studyapp 복사함
    
    - manager 추가, field 설정 추가 및 seed 파일 작성용

--- aid_web/studyapp/models.py
+++ aid_web/studyapp/models.py
@@ -1,1 +1,24 @@
-# Create your models here.
+from django.db import models
+from django.db.models import Model
+from userapp.models import User
+
+# class StudyManager():
+#     def create_study(self, study_name, study_description, study_link,
+#                      status, leader, users, img_url, created_at):
+
+
+class Study(Model):
+    study_name = models.CharField(max_length=50)
+    study_description = models.CharField(max_length=300)
+    study_link = models.CharField(max_length=500, null=True)
+    status = models.IntegerField()
+    leader = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, related_name="leader")
+    users = models.ManyToManyField(User)  # realted name
+    img_url = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.study_name
+
+    class Meta:
+        db_table = "Study"
