d54973b606e473a0b94f36a19c26b2f0257e7da8
june222 <0119299@gmail.com>
2024-01-04 22:19:47 +0900

    feat: studyapp modeling, 더미 데이터
    - 스터디 field에서 이름에는 unique, blank 추가
    
    - 소개에 blank 추가
    
    - status는 기본값 0
    
    - seed.py에는 주석 제거
    
    issue #25

--- aid_web/seeding/management/commands/custom_seed.py
+++ aid_web/seeding/management/commands/custom_seed.py
@@ -1,7 +1,9 @@
+from random import randint
 from typing import Any
 
 from django.core.management.base import BaseCommand, CommandParser
 from django_seed import Seed
+from studyapp.models import Study
 from userapp.models import User
 
 
@@ -17,23 +19,11 @@ class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any):
         options.get("superuser")
         user = options.get("user")
-        options.get("study")
+        study = options.get("study")
 
         # 한국 지역에 적용되는 값을 가져옴
         # faker docs Link: https://faker.readthedocs.io/en/master/locales/ko_KR.html
         seeder = Seed.seeder(locale="ko_KR")
-
-        # superuser 데이터를 만들 때는
-        # seeder.add_entity(
-        #     User,
-        #     superuser,
-        #     {
-        #         "nick_name": lambda x: seeder.faker.unique.name_female(),
-        #         "email": lambda x: seeder.faker.unique.email(),
-        #         "password": lambda x: seeder.faker.msisdn(),
-        #         "is_admin": lambda x: True,
-        #     },
-        # )
 
         seeder.add_entity(
             User,
@@ -47,14 +37,17 @@ class Command(BaseCommand):
         )
         # Django Seed 활용하여 FK 포함한 모델의 test data 생성하기
         # Link: https://ninefloor-design.tistory.com/323
-        # seeder.add_entity(
-        #     Study,
-        #     study,
-        #     {
-        #         "study_name": lambda x: seeder.faker.bs(),
-        #         "study_description": lambda x: seeder.faker.catch_phrase(),
-        #         "study_link": lambda x: seeder.faker.url(),
-        #         "status": lambda x: randint(0, 2),
-        #     },
-        # )
+
+        seeder.add_entity(
+            Study,
+            study,
+            {
+                "study_name": lambda x: seeder.faker.unique.bs(),
+                "study_description": lambda x: seeder.faker.catch_phrase(),
+                "study_link": lambda x: seeder.faker.url(),
+                "status": lambda x: randint(0, 2),
+                "img_url": lambda x: seeder.faker.image_url(),
+            },
+        )
+
         seeder.execute()
