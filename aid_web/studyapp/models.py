67338a678b04ca6dc4f23fc908c4d1cd08c99726
june222 <0119299@gmail.com>
2024-01-04 22:29:39 +0900

    style: OneToOne->ForeignKey
    - leader의 field Type을 수정함
    
    issue #28

--- aid_web/studyapp/models.py
+++ aid_web/studyapp/models.py
@@ -8,7 +8,7 @@ class Study(Model):
     study_description = models.CharField(max_length=300, blank=True)
     study_link = models.CharField(max_length=500, null=True)
     status = models.IntegerField(default=0)
-    leader = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, related_name="leader")
+    leader = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="leader")
     users = models.ManyToManyField(User)  # realted name
     img_url = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now=True)
