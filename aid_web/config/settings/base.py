10c467980a4ac967cbfc872ce514e98452e6b091
june222 <0119299@gmail.com>
2024-01-07 19:52:17 +0900

    feat: projectapp + seed
    - projectapp 모델링함
    
    - 필요한 seed 파일과 빌드 작성함.
    
    - 다만
    
    issue #31

--- aid_web/config/settings/base.py
+++ aid_web/config/settings/base.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "userapp.apps.UserappConfig",
     "studyapp.apps.StudyappConfig",
+    "projectapp.apps.ProjectappConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- aid_web/projectapp/models.py
+++ aid_web/projectapp/models.py
@@ -1,1 +1,19 @@
-# Create your models here.
+from django.db import models
+from django.db.models import Model
+from userapp.models import User
+
+
+class Project(Model):
+    project_name = models.CharField(max_length=50, unique=True, blank=True)
+    project_description = models.CharField(max_length=300, blank=True)
+    project_link = models.CharField(max_length=500, null=True)
+    status = models.IntegerField(editable=True, default=0)
+    users = models.ManyToManyField(User)
+    img_url = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.project_name
+
+    class Meta:
+        db_table = "Project"
