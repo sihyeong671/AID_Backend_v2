35e5480115947caabbbc086839d19469d6cb31e4
JongMok Lee <lijm1358@gmail.com>
2024-01-07 23:59:44 +0900

    feat: add study app urls

--- aid_web/config/urls.py
+++ aid_web/config/urls.py
@@ -19,4 +19,8 @@ from django.urls import include, path
 
 # from testapp.views import hello_rest_api
 
-urlpatterns = [path("admin/", admin.site.urls), path("userapp/", include("userapp.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("userapp/", include("userapp.urls")),
+    path("studyapp/", include("studyapp.urls")),
+]
