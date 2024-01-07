8c63e2aba07596b867658f1a6eb12a7e880c7820
JongMok Lee <lijm1358@gmail.com>
2024-01-08 00:03:56 +0900

    feat: add basic CRUD with ModelViewSet

--- aid_web/studyapp/views.py
+++ aid_web/studyapp/views.py
@@ -1,1 +1,20 @@
 # Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from .models import Study
+from .permissions import IsOwnerOfStudyOrReadOnly
+from .serializer import StudySerializer
+
+
+class StudyViewSet(viewsets.ModelViewSet):
+    queryset = Study.objects.all()
+    serializer_class = StudySerializer
+
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOfStudyOrReadOnly]
+
+    # override
+    def perform_create(self, serializer):
+        serializer.save(leader=self.request.user, users=[self.request.user])
+
+    # Q. ModelViewSet에 있는 update 사용 시 leader, users에 대한 정보를 넘기는 게 가능한가?
