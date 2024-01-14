904b0a0c007168cfbafd2c90191207d3e53f2f56
JongMok Lee <lijm1358@gmail.com>
2024-01-14 13:21:03 +0900

    feat: add custom api for quit, join study

--- aid_web/studyapp/views.py
+++ aid_web/studyapp/views.py
@@ -1,9 +1,11 @@
 # Create your views here.
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .models import Study
-from .permissions import IsOwnerOfStudyOrReadOnly
+from .permissions import IsOwnerOfStudyOrAdmin
 from .serializer import StudySerializer
 
 
@@ -11,10 +13,35 @@ class StudyViewSet(viewsets.ModelViewSet):
     queryset = Study.objects.all()
     serializer_class = StudySerializer
 
-    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOfStudyOrReadOnly]
+    permission_classes = [IsAuthenticated, IsOwnerOfStudyOrAdmin]
 
     # override
     def perform_create(self, serializer):
         serializer.save(leader=self.request.user, users=[self.request.user])
 
-    # Q. ModelViewSet에 있는 update 사용 시 leader, users에 대한 정보를 넘기는 게 가능한가?
+    @action(detail=True, methods=["patch"], permission_classes=[IsAuthenticated])
+    def quit(self, request, pk):
+        instance = self.get_object()
+        instance.users.remove(request.user)
+        if instance.leader == request.user:
+            instance.leader = None
+        instance.save()
+
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=["patch"], permission_classes=[IsAuthenticated])
+    def join(self, request, pk):
+        instance = self.get_object()
+        if instance.status != Study.OPENED:
+            return Response({"detail": "Study not available"}, status=406)
+        if instance.users.filter(id=request.user.id).exists():
+            return Response({"detail": "User already joined study"}, status=403)
+        # if instance.users_waiting.filter(id=request.user.id).exists():
+        #     return Response({"detail": "User already exists in users_waiting"}, status=403)
+        # instance.users_waiting.add(request.user)
+        instance.users.add(request.user)
+        instance.save()
+
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
