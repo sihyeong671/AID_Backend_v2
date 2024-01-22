fb369e7c4246055f5c895d0265a894590be833dd
JongMok Lee <lijm1358@gmail.com>
2024-01-22 23:13:21 +0900

    feat: study url renaming

--- aid_web/config/urls.py
+++ aid_web/config/urls.py
@@ -22,7 +22,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("studyapp/", include("studyapp.urls")),
+    path("api/", include("studyapp.urls")),
     path("api/", include("userapp.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
