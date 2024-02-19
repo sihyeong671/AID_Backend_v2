b5a51fdd2bd5c46d40b2b9e772c79f3fa9a4d8cf
sihyeong671 <bshlab671@naver.com>
2024-02-19 22:43:46 +0900

    feat: faq api

---
+++ aid_web/faqapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import FAQAPIView, FAQListAPIView
+
+urlpatterns = [
+    path("faq", FAQListAPIView.as_view()),
+    path("faq", FAQAPIView.as_view()),
+]
