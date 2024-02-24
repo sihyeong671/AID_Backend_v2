c1e2c705d175fad25136af3228cb61534a34c990
Ubuntu <ubuntu@ip-172-31-31-167.us-east-2.compute.internal>
2024-02-24 12:29:27 +0000

    feat: add faq admin

--- aid_web/faqapp/admin.py
+++ aid_web/faqapp/admin.py
@@ -1,7 +1,22 @@
 # Register your models here.
 
+from django import forms
 from django.contrib import admin
 
 from .models import FAQ
 
-admin.site.register(FAQ)
+
+class MyForm(forms.ModelForm):
+    class Meta:
+        widgets = {
+            "title": forms.TextInput(attrs={"size": "20"}),
+            "content": forms.Textarea(attrs={"cols": "40", "rows": "10", "class": "vLargeTextField"}),
+            "category": forms.TextInput(attrs={"size": "20"}),
+        }
+
+
+class FaqAdminForm(admin.ModelAdmin):
+    form = MyForm
+
+
+admin.site.register(FAQ, FaqAdminForm)
