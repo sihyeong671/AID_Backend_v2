33b98bf11540c42acdbc54273bcf974f7294b933
lijm1358 <lijm1358@gmail.com>
2024-02-24 12:51:52 +0000

    feat: change study model field

--- aid_web/studyapp/models.py
+++ aid_web/studyapp/models.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.db.models import IntegerChoices, Model
 
@@ -5,22 +6,31 @@ from django.db.models import IntegerChoices, Model
 class Study(Model):
     # https://www.b-list.org/weblog/2007/nov/02/handle-choices-right-way/
     class StatusType(IntegerChoices):
-        OPENED = 0
-        CLOSED = 1
+        NOT_STARTED = 0
+        ONGOING = 1
         FINISHED = 2
+
+    class StudyType(IntegerChoices):
+        PROJECT = 0
+        STUDY = 1
+        COMPETITION = 2
 
     study_name = models.CharField(max_length=50, unique=True)
     study_description = models.CharField(max_length=300, blank=True)
     study_link = models.CharField(max_length=500, null=True)
-    status = models.IntegerField(choices=StatusType.choices, default=StatusType.OPENED)
+    status = models.IntegerField(choices=StatusType.choices, default=StatusType.NOT_STARTED)
+    study_type = models.IntegerField(choices=StudyType.choices, default=StudyType.STUDY)
     max_participants = models.PositiveIntegerField(default=0)
     leader = models.CharField(max_length=50, null=True)
+    participants = ArrayField(models.CharField(max_length=50), blank=True, null=True)
     img_url = models.ImageField(upload_to="study_image/", default=None)
     created_at = models.DateTimeField(auto_now=True)
+    study_start = models.DateTimeField(null=True)
+    study_end = models.DateTimeField(null=True)
 
     def __str__(self):
         return self.study_name
 
     class Meta:
         db_table = "Study"
-        get_latest_by = ("status", "created_at")
+        get_latest_by = ("status", "study_start")
--- aid_web/studyapp/serializer.py
+++ aid_web/studyapp/serializer.py
@@ -1,65 +1,12 @@
-from drf_spectacular.utils import OpenApiExample, extend_schema_serializer
 from rest_framework import serializers
 
 from .models import Study
 
 
-@extend_schema_serializer(
-    examples=[
-        OpenApiExample(
-            "스터디 정보",
-            summary="스터디 정보",
-            description="스터디 정보 출력",
-            value={
-                "id": 1,
-                "status_readable": "Opened",
-                "leader": {"id": 1, "nick_name": "testname1", "email": "testname1@example.com"},
-                "users": [
-                    {"study_name": "study1", "user_email": "testname1@example.com", "user_id": 1, "is_approve": True}
-                ],
-                "study_name": "study1",
-                "study_description": "testdescription1",
-                "study_link": None,
-                "status": 0,
-                "img_url": "test",
-                "created_at": "2024-01-01T00:00:00.000000Z",
-            },
-            request_only=False,
-            response_only=True,
-        ),
-        OpenApiExample(
-            "스터디 생성",
-            summary="스터디 생성",
-            description="스터디 생성 및 수정용 response body",
-            value={
-                "study_name": "test study name",
-                "study_description": "test study description",
-                "status": 0,
-                "study_url": "http://test.study.url/",
-                "img_url": "testimg.jpg",
-            },
-            request_only=True,
-            response_only=False,
-        ),
-        OpenApiExample(
-            "스터디 수정",
-            summary="스터디 수정 summary",
-            description="스터디 수정 description",
-            value={
-                "study_name": "modified study name",
-                "study_description": "modified study description",
-                "status": 1,
-                "study_url": "http://modified.study.url/",
-                "img_url": "modifiedimg.jpg",
-            },
-            request_only=True,
-            response_only=False,
-        ),
-    ]
-)
 class StudySerializer(serializers.ModelSerializer):
-    status_readable = serializers.ChoiceField(
-        source="get_status_display", choices=Study.StatusType.choices, read_only=True
+    status = serializers.ChoiceField(source="get_status_display", choices=Study.StatusType.choices, read_only=True)
+    study_type = serializers.ChoiceField(
+        source="get_study_type_display", choices=Study.StudyType.choices, read_only=True
     )
 
     class Meta:
