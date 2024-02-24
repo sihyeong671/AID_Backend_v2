c8677ca4c9cf376b3460f735d147c2b37a3a4824
Ubuntu <ubuntu@ip-172-31-31-167.us-east-2.compute.internal>
2024-02-24 12:31:36 +0000

    feat: change study api to only have GET

--- aid_web/studyapp/models.py
+++ aid_web/studyapp/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.db.models import IntegerChoices, Model
-from userapp.models import User
 
 
 class Study(Model):
@@ -14,10 +13,9 @@ class Study(Model):
     study_description = models.CharField(max_length=300, blank=True)
     study_link = models.CharField(max_length=500, null=True)
     status = models.IntegerField(choices=StatusType.choices, default=StatusType.OPENED)
-    leader = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="leader")
-    users = models.ManyToManyField(User, through="StudyUserRelation")  # realted name
-    # users_waiting = models.ManyToManyField(User, blank=True, related_name="users_waiting")
-    img_url = models.CharField(max_length=500)
+    max_participants = models.PositiveIntegerField(default=0)
+    leader = models.CharField(max_length=50, null=True)
+    img_url = models.ImageField(upload_to="study_image/", default=None)
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -26,13 +24,3 @@ class Study(Model):
     class Meta:
         db_table = "Study"
         get_latest_by = ("status", "created_at")
-
-
-class StudyUserRelation(models.Model):
-    # 스터디 참여 여부 및 확장성을 위한 중간 테이블 명시.
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    study = models.ForeignKey(to=Study, on_delete=models.CASCADE)
-    is_approve = models.BooleanField(default=False)
-
-    class Meta:
-        db_table = "study_user"
--- aid_web/studyapp/serializer.py
+++ aid_web/studyapp/serializer.py
@@ -1,52 +1,7 @@
-from typing import Any
-
 from drf_spectacular.utils import OpenApiExample, extend_schema_serializer
 from rest_framework import serializers
-from userapp.models import User
 
-from .models import Study, StudyUserRelation
-
-# from .docs import study_extend_schema_serializer
-
-# https://www.django-rest-framework.org/api-guide/relations/
-
-# CRUD별로 다른 serializer를 써야할 때
-# https://velog.io/@haremeat/Django-%ED%95%98%EB%82%98%EC%9D%98-APIView%EC%97%90%EC%84%9C-Serializerclass%EA%B0%80-%EB%8B%A4%EB%A5%BC-%EB%95%8C
-# https://github.com/encode/django-rest-framework/issues/1563
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ("id", "nick_name", "email")
-
-
-@extend_schema_serializer(
-    many=True,
-    examples=[
-        OpenApiExample(
-            "스터디 내 사용자 정보",
-            summary="스터디 내 사용자 정보",
-            value={
-                "study_name": "study1",
-                "user_email": "testname@example.com",
-                "user_id": 1,
-                "is_approve": True,
-            },
-            request_only=False,
-            response_only=True,
-        )
-    ],
-)
-class StudyUserSerializer(serializers.Serializer):
-    study_name = serializers.CharField(source="study.study_name")
-    user_email = serializers.CharField(source="user.email")
-    user_id = serializers.IntegerField(source="user.id")
-    is_approve = serializers.BooleanField()
-
-    class Meta:
-        model = StudyUserRelation
-        fields = ("study_name", "user_email", "user_id", "is_approve")
+from .models import Study
 
 
 @extend_schema_serializer(
@@ -106,18 +61,6 @@ class StudySerializer(serializers.ModelSerializer):
     status_readable = serializers.ChoiceField(
         source="get_status_display", choices=Study.StatusType.choices, read_only=True
     )
-    leader = UserSerializer(many=False, read_only=True)
-    users = StudyUserSerializer(many=True, read_only=True, source="studyuserrelation_set")
-    # users_waiting = UserSerializer(many=True, read_only=True)
-
-    def create(self, validated_data: dict[str, Any]) -> Study:
-        leader = validated_data["leader"]
-        instance = Study.objects.create(**validated_data)
-
-        studyuserrelation_set = [StudyUserRelation(user_id=leader.id, study_id=instance.id, is_approve=True)]
-        instance.studyuserrelation_set.add(*studyuserrelation_set, bulk=False)
-
-        return instance
 
     class Meta:
         model = Study
