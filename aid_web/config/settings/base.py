1c8c56a02adceb3ce6da7dc441dccf770d33f104
sihyeong671 <bshlab671@naver.com>
2024-03-24 18:34:03 +0900

    fix: seeding app 삭제

--- aid_web/config/settings/base.py
+++ aid_web/config/settings/base.py
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",  # Django RestFrameWork 사용"
-    "seeding",  # 더미 데이터 생성용(폴더 이름)
     "drf_spectacular",
     "corsheaders",
     "storages",
